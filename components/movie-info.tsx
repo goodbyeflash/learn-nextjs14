a33c92cc1e4fd3149fc4c291f1496b0f4d87a260
goodbyeflash <admin@goodbye-flash.com>
2024-12-23 21:40:26 +0900

    3.5 Deployment

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,14 +1,31 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   console.log(`Fetching movies : ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("something broke...");
-  // const response = await fetch(`${API_URL}/${id}`);
-  // return response.json();
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  // throw new Error("something broke...");
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h1>{movie.title}</h1>;
+  return (
+    <div className={styles.container}>
+      <img
+        className={styles.poster}
+        src={movie.poster_path}
+        alt={movie.title}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
